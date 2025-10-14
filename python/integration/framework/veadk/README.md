638c432d02dcd7914b1022374fa4eebfec19cd44
jiangqi.rrt <jiangqi.rrt@bytedance.com>
2025-10-14 19:40:49 +0800

    add veadk demo

---
+++ python/integration/framework/veadk/README.md
@@ -0,0 +1,14 @@
+# How to Run
+
+# Set global environment variable
+- MODEL_AGENT_API_KEY=***
+
+use ark model api key, refer: https://www.volcengine.com/docs/82379/1361424
+
+# Set CozeloopExporterConfig in CozeloopExporter
+- space_id="***""
+- token="***"
+
+space_id is work_space_id in cozeloop, from https://loop.coze.cn/
+
+token is pat token or sat token, which has trace upload permission for this spaceID, like pat_*** or sat_***, reference doc: https://loop.coze.cn/open/docs/cozeloop/authentication-for-sdk
---
+++ python/integration/framework/veadk/otel_veadk.py
@@ -0,0 +1,44 @@
+import os
+import asyncio
+
+from veadk import Agent, Runner
+from veadk.memory.short_term_memory import ShortTermMemory
+from veadk.tools.demo_tools import get_city_weather
+from veadk.tracing.telemetry.exporters.cozeloop_exporter import CozeloopExporter, CozeloopExporterConfig
+from veadk.tracing.telemetry.opentelemetry_tracer import OpentelemetryTracer
+from opentelemetry import trace
+
+os.environ["MODEL_AGENT_API_KEY"] = "***"  # use ark model api key, refer: https://www.volcengine.com/docs/82379/1361424
+
+exporters = [CozeloopExporter(
+    config=CozeloopExporterConfig(
+        space_id="***",
+        token="***",
+    )
+)]
+tracer = OpentelemetryTracer(exporters=exporters)  # init veadk opentelemetry tracer
+otel_raw_tracer = trace.get_tracer_provider().get_tracer(__name__)  # get global opentelemetry tracer for custom span
+
+agent = Agent(
+    name="chat_robot",
+    description="A robot talk with user.",
+    instruction="Talk with user friendly.",
+    tools=[get_city_weather],
+    tracers=[tracer],
+    model_name="doubao-1-5-pro-32k-250115" # use your model name, like doubao-1-5-pro-32k-250115
+)
+
+session_id = "session_id"
+
+runner = Runner(
+    agent=agent,
+    short_term_memory=ShortTermMemory()
+)
+
+prompt = "How is the weather like in Beijing? Besides, tell me which tool you invoked."
+
+# set custom span
+with otel_raw_tracer.start_as_current_span("root_span") as span:
+    span.set_attribute("cozeloop.span_type", "custom")
+    # start veadk runner
+    asyncio.run(runner.run(messages=prompt, session_id=session_id))
